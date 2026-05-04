a1b6bfd4d05bd1fe9b57e72154cfb88c04d90684
Omarboraie2003 <omarboraie2003@gmail.com>
2026-05-05 00:35:46 +0300

    feat: install and setup sonner for notifications

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import { Provider } from 'react-redux';
 import { BrowserRouter } from 'react-router-dom';
+import { Toaster } from 'sonner';
 import store from './app/store';
 import AppRoutes from './routes/AppRoutes';
 
 function App() {
   return (
     <Provider store={store}>
+      <Toaster position="top-center" richColors />
       <BrowserRouter>
         <AppRoutes />
       </BrowserRouter>
